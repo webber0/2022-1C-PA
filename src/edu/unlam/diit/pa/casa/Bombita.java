a83958af99e874419cff0846527cab4626ebd404
nicol <nicol@DESKTOP-PVJHDJP>
2022-04-20 22:13:31 -0300

    Correcciones varias

--- src/edu/unlam/diit/pa/casa/Bombita.java
+++ src/edu/unlam/diit/pa/casa/Bombita.java
@@ -38,5 +38,4 @@ public class Bombita {
 			return otra;
 		}
 	}
-
 }
--- src/edu/unlam/diit/pa/casa/Habitacion.java
+++ src/edu/unlam/diit/pa/casa/Habitacion.java
@@ -2,19 +2,18 @@ package edu.unlam.diit.pa.casa;
 
 public class Habitacion {
 	protected String nombreHabitacion;
-	private Bombita[] bombitas; //Cambiarlo a ArrayList
-	
+	private Bombita[] bombitas; // Cambiarlo a ArrayList
+
 	public Habitacion(String nombreHabitacion, Bombita[] bombitas) {
 		this.nombreHabitacion = nombreHabitacion;
 		this.bombitas = bombitas;
 	}
-		
+
 	public Bombita buscarBombillaDeMayorConsumo() {
-		Bombita bombitaMayorConsumo=bombitas[0];
-		for(int i=1;i<bombitas.length;i++) {
-			bombitaMayorConsumo=bombitas[i].obtenerBombitaDeMayorConsumo(bombitaMayorConsumo);
+		Bombita bombitaMayorConsumo = bombitas[0];
+		for (int i = 1; i < bombitas.length; i++) {
+			bombitaMayorConsumo = bombitas[i].obtenerBombitaDeMayorConsumo(bombitaMayorConsumo);
 		}
 		return bombitaMayorConsumo;
 	}
-
 }
