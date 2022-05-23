e22d76b66bce87b63ab51f96a113c70cf8f6fce7
nicol <nicol@DESKTOP-PVJHDJP>
2022-05-22 21:31:06 -0300

    Se agregan archivos de salida

--- oia/edu/unlam/diit/pa/JugandoConLetras/MainLetras.java
+++ oia/edu/unlam/diit/pa/JugandoConLetras/MainLetras.java
@@ -5,7 +5,7 @@ public class MainLetras {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Rapigrama rapi=null;
-		Archivo archivo=new Archivo("testing");
+		Archivo archivo=new Archivo("05_noEntraTodo");
 		rapi=archivo.leerArchivo();
 		rapi.resolver();
 		archivo.guardarArchivo(rapi);
